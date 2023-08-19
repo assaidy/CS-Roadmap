2a46142ea28a4f4db5aafbfae0eb17cff513035f
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-08-19 16:27:30 +0300

    modified:   main.cpp 	deleted:    ../projects/guess-the-number 	modified:   ../projects/guess-the-number.cpp

--- CPP/cpp-primer/main.cpp
+++ CPP/cpp-primer/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <ostream>
 #include <string>
 
 int main() {
     int i{10};
     const int c{20};
-    int *const cptr = &i; // const pointer
+    int *const cptr = &i;        // const pointer
     const int *const ccptr = &c; // const pointer to a const
 
     return 0;
 }
-
