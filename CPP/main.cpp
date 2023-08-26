a60b14d08443d665ed2c62e4257fad2132d1bda7
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-08-26 15:50:40 +0300

    modified:   CPP/main 	modified:   CPP/main.cpp 	modified:   CPP/wee.h

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 
 int main() {
-    int size{};
-    std::cin >> size;
-
-    int nums[size];
-    int freq_arr[5001]{};
-
-    // fill and count freq
-    for (int i = 0; i < size; i++) {
-        std::cin >> nums[i];
-        freq_arr[nums[i]]++;
-    }
-
-    // count frequency sort
-    for (int i = 0; i < 5001; i++) {
-        while (freq_arr[i]--)
-            std::cout << i << ' ';
-    }
-    std::cout << '\n';
 
     return 0;
 }
