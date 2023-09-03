a1b99b0dea99d2b13b310a0381b46bb959ed86f7
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-09-03 11:39:57 +0300

    modified:   main 	modified:   main.cpp

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
 
 
 int main() {
+    std::vector<int> v;
+    v.reserve(50000); // reserve an initial capacity with 50000
+    std::cout << v.capacity(); // return the capacity 50000
+
 
     return 0;
 }
