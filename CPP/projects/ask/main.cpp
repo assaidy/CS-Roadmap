8b09cedbd0f2c0f68f78d81ccaf6871aec05e004
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-09-07 10:14:38 +0300

    modified:   main 	modified:   main.cpp

--- CPP/projects/ask/main.cpp
+++ CPP/projects/ask/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <assert.h>
 
 /////////////////////// Helper Functions /////////////////////////////
 std::vector<std::string> SplitString(std::string line, char delimiter = ',') {
@@ -89,6 +90,134 @@ int ReadMenu(std::vector<std::string> choices) {
     return ReadInt(1, choices.size());
 }
 //////////////////////////////////////////////////////////////////////
+
+struct Question {
+    int question_id;
+    int parent_question_id; // -1 => the first question in the thread
+    int from_user_id;
+    int to_user_id;
+    int is_anonymous_question; // 0 or 1
+    std::string question_txt;
+    std::string question_answer;
+
+    Question() {
+        question_id = parent_question_id = from_user_id = to_user_id = -1;
+        is_anonymous_question = 1;
+    }
+
+    Question(std::string line) {
+        std::vector<std::string> substrs{ SplitString(line) };
+        assert(int(substrs.size()) == 7);
+
+        question_id = ToInt(substrs.at(0));
+        parent_question_id = ToInt(substrs.at(1));
+        from_user_id = ToInt(substrs.at(2));
+        to_user_id = ToInt(substrs.at(3));
+        is_anonymous_question = ToInt(substrs.at(4));
+        question_txt = substrs.at(5);
+        question_answer = substrs.at(6);
+    }
+
+    std::string ToString() {
+        std::ostringstream oss;
+        oss << question_id << "," << parent_question_id << "," << from_user_id << "," << to_user_id << "," << is_anonymous_question << "," << question_txt << "," << question_answer;
+        return oss.str();
+    }
+
+    void PrintToQuestions() {
+        std::string prefix{ "" };
+
+        if (prefix != "") {
+            prefix = "\tThread: ";
+        }
+
+        std::cout << prefix << "Question id (" << question_id << ")";
+        if (!is_anonymous_question) {
+            std::cout << " from user id (" << from_user_id << ")";
+        }
+        std::cout << "\tQuestion: " << question_txt << '\n';
+
+        if (question_answer != "") {
+            std::cout << prefix << "\tAnswer: " << question_answer << '\n';
+        }
+        std::cout << '\n';
+    }
+
+    void PrintFromQuestions() {
+        std::cout << "Question id (" << question_id << ")";
+
+        if (!is_anonymous_question)
+            std::cout << " !AQ";
+
+        std::cout << " to user id (" << to_user_id << ")";
+        std::cout << "\tQuestion: " << question_txt;
+
+        if (question_answer != "")
+            std::cout << "\tAnswer: " << question_answer << '\n';
+        else
+            std::cout << "\tNot Answered Yet\n";
+    }
+
+    void PrintFeedQuestions() {
+        if (parent_question_id != -1) {
+            std::cout << "Thread Parent Question ID (" << parent_question_id << ")";
+        }
+        std::cout << " Question ID (" << question_id << ")";
+
+        if (!is_anonymous_question) {
+            std::cout << " from user id (" << from_user_id << ")";
+        }
+        std::cout << " to user id (" << to_user_id << ")";
+
+        std::cout << "\tQuestion: " << question_txt << '\n';
+        if (question_answer != "") {
+            std::cout << "\tAnswer: " << question_answer << '\n';
+        }
+    }
+
+};
+
+struct User {
+    int user_id;
+    std::string user_name;
+    std::string password;
+    std::string name;
+    std::string email;
+    int allow_anonymous_questions; // 0 or 1
+
+    std::vector<int> questions_id_from_me;
+    // From question id to list of questions IDS on this question (thread questions) - For this user
+    std::map<int, std::vector<int>> questionid_questionidsThead_to_map;
+
+    User() {
+        user_id = allow_anonymous_questions = -1;
+    }
+
+    User(std::string line) {
+        std::vector<std::string> substrs{ SplitString(line) };
+        assert(int(substrs.size()) == 6);
+
+        user_id = ToInt(substrs.at(0));
+        user_name = substrs.at(1);
+        password = substrs.at(2);
+        name = substrs.at(3);
+        email = substrs.at(4);
+        allow_anonymous_questions = ToInt(substrs.at(5));
+    }
+
+    std::string ToString() {
+        std::ostringstream oss;
+        oss << user_id << "," << user_name << "," << password << "," << name << "," << email << "," << allow_anonymous_questions;
+
+        return oss.str();
+    }
+
+    void Print() {
+        std::cout << "User " << user_id << ", " << user_name << " " << password << ", " << name << ", " << email << "\n";
+    }
+};
+
+
 
 
 
