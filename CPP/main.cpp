10574535a91e1222312c5a9cdcc77e0e6e43e0ef
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-09-07 11:31:12 +0300

    modified:   main.cpp

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
-#include <sstream>
-#include <fstream>
 
 int main() {
-    std::ostringstream oss;
-    oss << "Ahmad ";
-    oss << 50;
-    oss << " years\n";
-
-    std::cout << oss.str();
 
     return 0;
 }
