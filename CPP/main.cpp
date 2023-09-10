8e7df9dd6ff30cd89d8d8b48059c470ebbd32510
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-09-10 20:33:27 +0300

    modified:   main.cpp

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -3,22 +3,8 @@
 #include <string>
 #include <vector>
 
-struct SpecialName {
-    std::string name = "Ahmad";
-    std::string &get_name() {return name;}
-    void print() {std::cout << name << '\n';}
-};
 
 int main() {
-    SpecialName name;
-    name.print();
-
-    std::string &str {name.get_name()};
-    str = "Ziad";
-    name.print();
-
-    name.get_name() = "Ali";
-    name.print();
 
 
     return 0;
