4f875368aa0845eb2c92a0cc1350dce77b456449
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-10-01 20:54:34 +0300

    modified:   main.cpp

--- DS/main.cpp
+++ DS/main.cpp
@@ -17,55 +17,7 @@ struct Node {
 };
 
 
-class LinkedList {
-private:
-    Node *head {};
-
-public:
-    // O(n) time, O(1) memroy
-    void print() {
-        for (Node *cur = head; cur; cur = cur->next) {
-            std::cout << cur->data << " ";
-        }
-        std::cout << "\n";
-    }
-
-    // O(1) time, O(1) memory
-    void add(int val) { // add() now do push_front()
-        Node *item = new Node(val);
-
-        if (!head)
-            head = item;
-        else
-         item->next = head, head = item;
-    }
-
-    // O(n) time, O(1) memory
-    Node *get_tail() {
-        for (Node *cur = head; cur; cur = cur->next)
-            if (!cur->next) return cur;
-        return nullptr;
-    }
-
-    ~LinkedList() {
-        Node *tmp = nullptr;
-        for (Node *cur = head; cur; cur = tmp) {
-            tmp = cur->next;
-            delete cur;
-        }
-    }
-};
-
-
 int main() {
-    LinkedList list;
-    list.add(10);
-    list.add(20);
-    list.add(30);
-
-    list.print();
-
-    std::cout << list.get_tail()->data << "\n";
 
     return 0;
 }
