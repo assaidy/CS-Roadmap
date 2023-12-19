6c671e0036bcca2139ddd43a5a4278fe11576129
assaidy <alsaidyxcode@gmail.com>
2023-12-19 06:30:20 +0200

    modify

--- CSharp/ConsoleApp1/Program.cs
+++ CSharp/ConsoleApp1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("hello, World");
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student student1 = new Student("ahmad");
+    }
+}
---
+++ CSharp/ConsoleApp1/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1;
+
+public class Student
+{
+    public Student(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+}
